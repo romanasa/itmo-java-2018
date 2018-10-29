74a7b3e837f6ad2362ffa8fedc151fc3b87b402e
romanasa <romanfml31@gmail.com>
2018-10-29 22:45:44 +0300

    add WordStatIndex and WordStatLineIndex

---
+++ java/WordStatIndex.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class WordStatIndex {
+    public static void main(String[] args) {
+
+        try (FastScanner scanner = new FastScanner(new FileInputStream(args[0]))) {
+
+            HashMap<String, ArrayList<Integer>> ind = new HashMap<>();
+            ArrayList<String> order = new ArrayList<>();
+
+            int num = 0;
+            while (scanner.hasNextLine()) {
+
+                while (scanner.hasNextInLine()) {
+                    String word = scanner.nextWord().toLowerCase();
+                    num++;
+                    if (!ind.containsKey(word)) {
+                        ind.put(word, new ArrayList<>());
+                        order.add(word);
+                    }
+                    ind.get(word).add(num);
+                }
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), StandardCharsets.UTF_8))) {
+                for (String word : order) {
+                    writer.write(word + " " + ind.get(word).size());
+                    for (Integer index : ind.get(word)) {
+                        writer.write(" " + index);
+                    }
+                    writer.newLine();
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Usage: java WordStatInput <input> <output>");
+            } catch (IOException e) {
+                System.out.println(e.toString());
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Usage: java WordStatInput <input> <output>");
+        } catch (FileNotFoundException e) {
+            System.out.println("No such file: " + args[0]);
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Unsupported Encoding");
+        } catch (IOException e) {
+            System.out.println("Error in reading");
+            System.out.println(e.toString());
+        }
+
+    }
+}
---
+++ java/WordStatLineIndex.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class WordStatLineIndex {
+    public static void main(String[] args) {
+
+        try (FastScanner scanner = new FastScanner(new FileInputStream(args[0]))) {
+
+            HashMap<String, ArrayList<Integer>> line = new HashMap<>();
+            HashMap<String, ArrayList<Integer>> ind = new HashMap<>();
+            ArrayList<String> order = new ArrayList<>();
+
+            int lineInd = 0;
+            while (scanner.hasNextLine()) {
+
+                int num = 0;
+                lineInd++;
+                while (scanner.hasNextInLine()) {
+                    String word = scanner.nextWord().toLowerCase();
+                    num++;
+                    if (!ind.containsKey(word)) {
+                        ind.put(word, new ArrayList<>());
+                        line.put(word, new ArrayList<>());
+                        order.add(word);
+                    }
+                    ind.get(word).add(num);
+                    line.get(word).add(lineInd);
+                }
+            }
+
+            Collections.sort(order);
+
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), StandardCharsets.UTF_8))) {
+                for (String word : order) {
+                    writer.write(word + " " + ind.get(word).size());
+                    for (int i = 0; i < ind.get(word).size(); i++) {
+                        writer.write(" " + line.get(word).get(i) + ":" + ind.get(word).get(i));
+                    }
+                    writer.newLine();
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Usage: java WordStatInput <input> <output>");
+            } catch (IOException e) {
+                System.out.println(e.toString());
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Usage: java WordStatInput <input> <output>");
+        } catch (FileNotFoundException e) {
+            System.out.println("No such file: " + args[0]);
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Unsupported Encoding");
+        } catch (IOException e) {
+            System.out.println("Error in reading");
+            System.out.println(e.toString());
+        }
+
+    }
+}
