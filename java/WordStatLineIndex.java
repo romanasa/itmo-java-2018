4896c1169b36310bd20a6584f791a6bd2c4919b9
romanasa <romanfml31@gmail.com>
2018-10-30 12:27:18 +0300

    optimize code style

--- java/WordStatLineIndex.java
+++ java/WordStatLineIndex.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 public class WordStatLineIndex {
     public static void main(String[] args) {
@@ -10,8 +8,7 @@ public class WordStatLineIndex {
         try (FastScanner scanner = new FastScanner(new FileInputStream(args[0]))) {
 
             HashMap<String, ArrayList<Integer>> line = new HashMap<>();
-            HashMap<String, ArrayList<Integer>> ind = new HashMap<>();
-            ArrayList<String> order = new ArrayList<>();
+            TreeMap<String, ArrayList<Integer>> ind = new TreeMap<>();
 
             int lineInd = 0;
             while (scanner.hasNextLine()) {
@@ -21,38 +18,35 @@ public class WordStatLineIndex {
                 while (scanner.hasNextInLine()) {
                     String word = scanner.nextWord().toLowerCase();
                     num++;
-                    if (!ind.containsKey(word)) {
-                        ind.put(word, new ArrayList<>());
-                        line.put(word, new ArrayList<>());
-                        order.add(word);
-                    }
+                    ind.putIfAbsent(word, new ArrayList<>());
+                    line.putIfAbsent(word, new ArrayList<>());
                     ind.get(word).add(num);
                     line.get(word).add(lineInd);
                 }
             }
 
-            Collections.sort(order);
-
             try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), StandardCharsets.UTF_8))) {
-                for (String word : order) {
+
+                for (String word : ind.keySet()) {
                     writer.write(word + " " + ind.get(word).size());
-                    for (int i = 0; i < ind.get(word).size(); i++) {
+                    for (int i = 0; i < (int) ind.get(word).size(); i++) {
                         writer.write(" " + line.get(word).get(i) + ":" + ind.get(word).get(i));
                     }
                     writer.newLine();
                 }
+
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Usage: java WordStatLineIndex <input> <output>");
-            } catch (UnsupportedEncodingException e) {
-                System.out.println("Unsupported Encoding");
+                System.out.println("Usage: java WordStatInput <input> <output>");
             } catch (IOException e) {
                 System.out.println(e.toString());
             }
 
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Usage: java WordStatLineIndex <input> <output>");
+            System.out.println("Usage: java WordStatInput <input> <output>");
         } catch (FileNotFoundException e) {
             System.out.println("No such file: " + args[0]);
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Unsupported Encoding");
         } catch (IOException e) {
             System.out.println("Error in reading");
             System.out.println(e.toString());
